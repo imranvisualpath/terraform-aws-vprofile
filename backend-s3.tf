aa525b7ad836ed0dee95abed339da799defd76f0
imran <imran@gmail.com>
2020-10-09 15:13:06 +0530

    vpc module

--- backend-s3.tf
+++ backend-s3.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-   bucket = "terra-vprofile-state11"
-   key = "terraform/backend"
-   region = "us-east-2"
+    bucket = "terra-vprofile-state11"
+    key    = "terraform/backend"
+    region = "us-east-2"
   }
 }
