14a8a754a87f1a383e361644c1ba590df03c2abd
imran <imran@gmail.com>
2020-10-09 19:04:42 +0530

    vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,9 @@
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
+  source = "terraform-aws-modules/vpc/aws"
 
   name = var.VPC_NAME
   cidr = var.VpcCIDR
+
   azs             = [var.Zone1, var.Zone2, var.Zone3]
   private_subnets = [var.PrivSub1CIDR, var.PrivSub2CIDR, var.PrivSub3CIDR]
   public_subnets  = [var.PubSub1CIDR, var.PubSub2CIDR, var.PubSub3CIDR]
@@ -16,6 +17,7 @@ module "vpc" {
     Terraform   = "true"
     Environment = "Prod"
   }
+
   vpc_tags = {
     Name = var.VPC_NAME
   }
