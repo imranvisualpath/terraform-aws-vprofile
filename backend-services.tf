f9c2e9ad432a89a0e3772e1ef0e35f9facdbcfac
imranvisualpath <72437104+imranvisualpath@users.noreply.github.com>
2024-11-26 11:36:09 +0530

    Update backend-services.tf

--- backend-services.tf
+++ backend-services.tf
@@ -14,14 +14,14 @@ resource "aws_elasticache_subnet_group" "vprofile-ecache-subgrp" {
 
 resource "aws_db_instance" "vprofile-rds" {
   allocated_storage      = 20
-  storage_type           = "gp2"
+  storage_type           = "gp3"
   engine                 = "mysql"
-  engine_version         = "5.7"
-  instance_class         = "db.t2.micro"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
   db_name                = var.dbname
   username               = var.dbuser
   password               = var.dbpass
-  parameter_group_name   = "default.mysql5.7"
+  parameter_group_name   = "default.mysql8.0.3"
   multi_az               = "false"
   publicly_accessible    = "false"
   skip_final_snapshot    = true
@@ -32,7 +32,7 @@ resource "aws_db_instance" "vprofile-rds" {
 resource "aws_elasticache_cluster" "vprofile-cache" {
   cluster_id           = "vprofile-cache"
   engine               = "memcached"
-  node_type            = "cache.t2.micro"
+  node_type            = "cache.t3.micro"
   num_cache_nodes      = 1
   parameter_group_name = "default.memcached1.6"
   port                 = 11211
@@ -43,7 +43,7 @@ resource "aws_elasticache_cluster" "vprofile-cache" {
 resource "aws_mq_broker" "vprofile-rmq" {
   broker_name        = "vprofile-rmq"
   engine_type        = "ActiveMQ"
-  engine_version     = "5.15.0"
+  engine_version     = "5.17.6"
   host_instance_type = "mq.t2.micro"
   security_groups    = [aws_security_group.vprofile-backend-sg.id]
   subnet_ids         = [module.vpc.private_subnets[0]]
